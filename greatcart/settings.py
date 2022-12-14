1e188144899f48a49c318da9f138090037828263
gvazco <gustavovazco@gmail.com>
2022-12-13 22:00:07 -0600

    🚧feat: forgot password whit secure validation

--- greatcart/settings.py
+++ greatcart/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+from django.contrib.messages import constants as messages
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -125,9 +126,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR /'static'
+STATIC_ROOT = BASE_DIR / 'static'
 STATICFILES_DIRS = [
-  'greatcart/static',
+    'greatcart/static',
 ]
 
 # AWS S3 Static Files Configuration
@@ -153,14 +154,13 @@ STATICFILES_DIRS = [
 # media files configuration
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR /'media'
+MEDIA_ROOT = BASE_DIR / 'media'
 
-from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
-  messages.ERROR: 'danger',
+    messages.ERROR: 'danger',
 }
 
-#SMTP configuration
+# SMTP configuration
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'gustavovazco@gmail.com'
